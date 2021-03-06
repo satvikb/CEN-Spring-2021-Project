814fabc24bb49feb8cb900bdb44a6e4c469fc816
Satvik Borra <borrasatvik@gmail.com>
2021-03-06 04:00:22 -0500

    Setup website header navigation and basic page switching

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,69 @@
+import NavigationButton from './NavigationButton';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from '../pages/Home'
+import OurTeam from '../pages/OurTeam'
+import Involvement from '../pages/Involvement'
+import Events from '../pages/Events'
+import Apply from '../pages/Apply'
+
+var buttons = [
+  {
+    "text":"Home",
+    "key":"home",
+    "url":"/"
+  },
+  {
+    "text":"Our Team",
+    "key":"ourteam",
+    "url":"/ourteam"
+  },
+  {
+    "text":"Involvement",
+    "key":"involvement",
+    "url":"/involvement"
+  },
+  {
+    "text":"Events",
+    "key":"events",
+    "url":"/events"
+  },
+  {
+    "text":"Apply",
+    "key":"apply",
+    "url":"/apply"
+  }
+]
+function NavigationBar(props) {
+  return (
+    <Router>
+      <div className="NavigationBar">
+        <p className="WebsiteHeader">Tamid UF</p>
+        {buttons.map((item, index) => {
+          return <NavigationButton key={item.key} text={item.text} url={item.url}></NavigationButton>
+        })}
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/ourteam">
+            <OurTeam />
+          </Route>
+          <Route path="/involvement">
+            <Involvement />
+          </Route>
+          <Route path="/events">
+            <Events />
+          </Route>
+          <Route path="/apply">
+            <Apply />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default NavigationBar;
---
+++ src/pages/Events.js
@@ -0,0 +1,9 @@
+function Events() {
+  return (
+    <div className="Events">
+        <p>Events</p>
+    </div>
+  );
+}
+
+export default Events;
