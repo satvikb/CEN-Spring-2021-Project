f2f44c243f0bda1dfec764a7309cb2434778085e
Satvik Borra <borrasatvik@gmail.com>
2021-03-22 12:08:39 -0400

    example content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,9 @@ function Home() {
 
       <div className="HomeContent">
         <p className="TextTitle">About Us</p>
-        <p className="TextDetail">Club about us text</p>
+        <p className="TextDetail">With unprecedented pacing in the world of technology comes a need to engage in business in new ways. Join us in developing solutions for fast-moving ventures and in meeting new challenges from experienced investors and entrepreneurs. We offer initiatives to engage in Israeli business and a strong network to aid in future development.
+
+TAMID is a comprehensive business and technology group that uses Israel as an economic lens to consult leading companies on the forefront of innovation.</p>
 
         <p className="TextTitle">Updates</p>
         <div className="UpdatesContainer">
