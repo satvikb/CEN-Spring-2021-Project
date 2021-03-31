c6d7c0695092d3b19bdecc77c373eec96d3cbb7b
josephbensabat <josephbensabat12@gmail.com>
2021-03-31 12:25:34 -0400

    Events changes

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,10 +1,19 @@
 import EventCard from "../components/EventCard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CardDeck from 'react-bootstrap/CardDeck';
+
+
 
 function Events() {
   return (
     <div className="Events">
-        <p>Events</p>
-        <EventCard title='Event Title' location='Event Location' date='Event Date' info='Event Info' />
+        <p>Tamid Group Events</p>
+        <CardDeck>
+            <EventCard title='Event Title' location='Event Location' date='Event Date' info='Event Info' />
+            <EventCard title='Event Title' location='Event Location' date='Event Date' info='Event Info' />
+            <EventCard title='Event Title' location='Event Location' date='Event Date' info='Event Info' />
+        </CardDeck>
+       
     </div>
   );
 }
