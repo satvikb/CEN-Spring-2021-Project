37fd2bff3425e6dd7b52efae74ab122d43d53701
rithikabikkavilli <rithikasravya@gmail.com>
2021-04-19 13:16:11 -0400

    updated our team page with headshots

--- src/pages/OurTeam.js
+++ src/pages/OurTeam.js
@@ -7,20 +7,22 @@ function OurTeam() {
 
     <div className="OurTeamBackgroundImage">
         <div className="BannerTitle">
-          <h1 className="BannerTitleText">Our Team</h1>
+          <h1 className="BannerTitleText">OUR TEAM</h1>
         </div>
       </div>
 
         <div className="EBoardCardRow">
-        <EBoardCard firstname = 'FirstName' lastname = 'LastName' position = 'Position'></EBoardCard>
-        <EBoardCard firstname = 'FirstName' lastname = 'LastName' position = 'Position'></EBoardCard>
-        <EBoardCard firstname = 'FirstName' lastname = 'LastName' position = 'Position'></EBoardCard>
+        <EBoardCard firstname = 'Blair' lastname = 'Wales' position = 'President'></EBoardCard>
+        <EBoardCard firstname = 'Jordana' lastname = 'Schube' position = 'Vice President'></EBoardCard>
+        <EBoardCard firstname = 'Jobeth' lastname = 'Liss' position = 'Director of Fund'></EBoardCard>
+        <EBoardCard firstname = 'Lauren' lastname = 'Hetzroni' position = 'Director of Consulting'></EBoardCard>
         </div>
 
         <div className="EBoardCardRow">
-        <EBoardCard firstname = 'FirstName' lastname = 'LastName' position = 'Position'></EBoardCard>
-        <EBoardCard firstname = 'FirstName' lastname = 'LastName' position = 'Position'></EBoardCard>
-        <EBoardCard firstname = 'FirstName' lastname = 'LastName' position = 'Position'></EBoardCard>
+        <EBoardCard firstname = 'Camila' lastname = 'Tarantini' position = 'Director of Programming'></EBoardCard>
+        <EBoardCard firstname = 'Shea' lastname = 'Berman' position = 'Director of Recruitment'></EBoardCard>
+        <EBoardCard firstname = 'William' lastname = 'Bussey' position = 'New Member Educator'></EBoardCard>
+        <EBoardCard firstname = 'Gabriella' lastname = 'Ben-Zion' position = 'Director of Marketing'></EBoardCard>
         </div>
       
     </div>
