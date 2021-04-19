80e48559242a3c1e45bd8086bddcd967b9fc7d9a
josephbensabat <josephbensabat12@gmail.com>
2021-04-19 18:00:29 -0400

    new pics and started footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import NavigationBar from './components/NavigationBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
         <NavigationBar text="Button"></NavigationBar>
-        <p>Footer</p>
+        
+        <Footer></Footer>
     </div>
+    
 
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+ import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// var style1 = {
+//     backgroundColor: "#42B5E8",
+//     borderTop: "1px solid #42B5E8",
+//     textAlign: "center",
+//     //padding: "20px",
+//     //position: "bottom",
+//     // left: "0",
+//     bottom: "0",
+//     //height: "60px",
+//     width: "100%",
+//     margintop: "-32767px;"
+// };
+
+class Footer extends React.Component
+{
+  render()
+  {
+    return (
+        <div class="jumbotron text-center"  style={
+            {backgroundColor: "#42B5E8"}
+          }>
+      </div>
+  
+    )
+  }
+}
+export default Footer;
+
+
+
+
+
