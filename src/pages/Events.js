b35e0c6e5d1107c290692ba0c9a96a3d93fddb1f
josephbensabat <josephbensabat12@gmail.com>
2021-04-19 12:16:01 -0400

    Event Page With Calendar Fixed

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -3,9 +3,13 @@ import ReactCalendar from "../components/ReactCalendar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CardDeck from 'react-bootstrap/CardDeck';
 
+
 import { db, auth, timestamp as fbTimestamp, timestampToString} from '../firebase.config';
 import React,{useState,useEffect} from 'react';
 
+var cardDeckStyle = {
+  padding: "20px"
+};
 
 function Events() {
   const [events,setEvents]=useState([])
@@ -35,7 +39,8 @@ function Events() {
           </div>
         </div>
 
-        <div className="col d-flex justify-content-center">
+        <div className="col d-flex justify-content-center" style={cardDeckStyle}>
+  
           <CardDeck>
           {
             events && events.map(event=>{
@@ -45,8 +50,8 @@ function Events() {
           </CardDeck>
         </div>
 
-        <ReactCalendar>
-        </ReactCalendar>
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23039BE5&amp;ctz=America%2FNew_York&amp;src=dGFtaWRhdHVmQGdtYWlsLmNvbQ&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%230B8043&amp;showTitle=0&amp;showPrint=1&amp;showNav=1&amp;showDate=1&amp;showTabs=1&amp;showCalendars=1&amp;showTz=1"  width="800" height="600" frameborder="0" scrolling="no"></iframe>
+
     </div>
   );
 
